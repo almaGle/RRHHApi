9509c7a08675ebdb48cc2a438cf0b8eb3ab572b3
almaGle <aglez337@alumnos.uaq.mx>
2024-10-22 11:45:29 -0600

    modificacion de la base de datos 2

--- config/database.js
+++ config/database.js
@@ -4,8 +4,8 @@ const util = require('util')
 const pool = mysql.createPool({
     connectionLimit: 10,
     host: 'host.docker.internal',
-    user: 'root',
-    password: '',
+    user: 'admin',
+    password: 'admin123',
     database: 'rrhh',
 })
 
